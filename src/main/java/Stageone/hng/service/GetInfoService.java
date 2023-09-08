302a3778d0d6d194379b06acecbb5e1aa15e491f
Igbokwe Victor <igbokwevictor122@gmail.com>
2023-09-08 22:05:47 +0100

    changes

--- src/main/java/Stageone/hng/service/GetInfoService.java
+++ src/main/java/Stageone/hng/service/GetInfoService.java
@@ -4,24 +4,30 @@ import Stageone.hng.entity.Response;
 import org.springframework.stereotype.Service;
 
 import java.time.DayOfWeek;
+import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 import java.time.format.DateTimeFormatter;
+import java.util.Random;
 
 @Service
 public class GetInfoService {
 
     public Response getInfoParam(String slack_name, String track){
         DayOfWeek currentDay = LocalDateTime.now().getDayOfWeek();
+        String githubRepoUrl = "https://github.com/meysdays/StageOneHNG";
+        String githubFileUrl = "https://github.com/meysdays/StageOneHNG/blob/master/src/main/java/Stageone/hng/HngApplication.java";
 
-        LocalDateTime utcTime = LocalDateTime.now(ZoneOffset.UTC);
-        String githubRepoUrl = "hsaknjdnjkjdsn";
-        String githubFileUrl = "hdcuasdhciad";
+        Instant currentInstant = Instant.now();
+        Random random = new Random();
+        int randomOffset = random.nextInt(241) - 120;
+        Instant utcTimeWithAccuracy = currentInstant.plusSeconds(randomOffset);
+        LocalDateTime utcLocalDateTime = utcTimeWithAccuracy.atOffset(ZoneOffset.UTC).toLocalDateTime();
 
         Response resp = new Response(
                 slack_name,
                 currentDay.toString(),
-                utcTime.format(DateTimeFormatter.ISO_DATE),
+                utcLocalDateTime.toString(),
                 track,
                 githubFileUrl,
                 githubRepoUrl,
